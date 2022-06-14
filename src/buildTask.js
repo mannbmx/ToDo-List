dc217f7dd73043c69872c97e4a223a4e8f6b0228
mannbmx <mannbmx@gmail.com>
2022-06-14 12:11:47 -0400

    Created form input for adding tasks

--- src/buildTask.js
+++ src/buildTask.js
@@ -13,7 +13,21 @@ const newTask = (title, priority, dueDate, Description, projectId) => {
 //Function creates new task
 function createTask(){
     //Dom selector gets values from for goes here
+    const taskTitle = document.querySelector('.taskTitle');
+    const taskPriority = document.querySelector('#priority');
+    const taskDueDate = document.querySelector('.dueDate');
+    const taskDescription = document.querySelector('.description');
 
-    const task = newTask();
+
+    const task = newTask(taskTitle.value, taskPriority.value, taskDueDate.value, taskDescription.value);
     taskList.push(task);
+
+    console.log(taskList);
+    closeForm();
 }
+
+function closeForm(){
+    document.querySelector('.taskFormCont').style.display = 'none';
+}
+
+export{createTask, taskList}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import {newProject, createProject, projectList} from "./buildProject";
+import {createTask, taskList} from "./buildTask";
 
 
 
 let temp = createProject('z')
 console.log(projectList)
 
+
+const newTaskButton = document.querySelector('.taskButton');
+newTaskButton.addEventListener('click', function(){createTask()});
+
+
+
+function openForm(){
+    document.querySelector('.taskFormCont').style.display = 'grid'
+}
+
+const openFormButton = document.querySelector('.newTodoButton');
+openFormButton.addEventListener('click', function(){openForm()})
 
